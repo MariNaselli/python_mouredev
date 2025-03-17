9e5c82bb66ddcf4ae99bbbeeee211acea684e4e1
Mariana Naselli <nasellimariana@outlook.com>
2025-03-17 16:01:31 +0100

    Inicializo repositorio

---
+++ hello.py
@@ -0,0 +1,28 @@
+# Esto es un comentario
+
+""" 
+Comentario en
+varias 
+lineas
+"""
+
+'''
+También comentario
+en varias
+lineas
+'''
+
+print("Hola")
+
+print ('Hola')
+
+#Consultar tipo de dato
+print(type("HOLA"))
+
+print(type(5))
+
+print(type(1.5))
+
+print(type(True))
+
+print(type(1 + 1j))
